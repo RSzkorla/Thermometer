dbd9c04d75c05ac4c30b16c81d0d5f6ac9c32650
RSzkorla <r.szkorla@gmail.com>
2018-05-04 09:39:15 +0200

    Klasa Config

---
+++ Thermometer/Models/Config.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Thermometer.Models
+{
+  public class Config
+  {
+    private static Config _instanceConfig;
+
+    public double LowerAlarmBorder { get; set; }
+    public double LoverWarnBorder { get; set; }
+    public double UpperWarnBorder { get; set; }
+    public double UpperAlarmBorder { get; set; }
+
+    public List<string> Emails { get; set; }
+    public List<string> PhoneNumbers { get; set; }
+    private Config()
+    {
+      LowerAlarmBorder = 10;
+      LoverWarnBorder = 11;
+      UpperWarnBorder = 29;
+      UpperAlarmBorder = 30;
+    }
+
+    public void SaveConfig()
+    {
+      
+    }
+
+
+    public void RestoreDefaults()
+    {
+      LowerAlarmBorder = 10;
+      LoverWarnBorder = 11;
+      UpperWarnBorder = 29;
+      UpperAlarmBorder = 30;
+    }
+    public static Config InstanceConfig
+    {
+      get
+      {
+        if (_instanceConfig == null)
+          _instanceConfig = new Config();
+        return _instanceConfig;
+      }
+    }
+
+  }
+}
