c1dcb2860682a980b4fa95a75457784a8ba2b078
RSzkorla <r.szkorla@gmail.com>
2018-06-04 18:33:45 +0200

    dodanie try catcha do gpioSensor

--- Thermometer/Models/GpioSensor.cs
+++ Thermometer/Models/GpioSensor.cs
@@ -8,16 +8,23 @@ namespace Thermometer.Models
     private string _devicePath = "/sys/bus/w1/devices/28-000008b8db23";
     public double GetTemperature()
     {
-      StreamReader str = new StreamReader(_devicePath + "/w1_slave");
-      string content = string.Empty;
-      while (!str.EndOfStream)
+      try
       {
-        content = str.ReadLine();
+        StreamReader str = new StreamReader(_devicePath + "/w1_slave");
+        string content = string.Empty;
+        while (!str.EndOfStream)
+        {
+          content = str.ReadLine();
+        }
+        string tempString = content.Substring(content.Length - 6, 6).Replace("=", "");
+        int tempInt = Convert.ToInt32(tempString);
+        double temp = tempInt / 1000.0;
+        return temp;
       }
-      string tempString = content.Substring(content.Length - 6, 6).Replace("=", "");
-      int tempInt = Convert.ToInt32(tempString);
-      double temp = tempInt / 1000.0;
-      return temp;
+      catch (Exception)
+      {
+        return double.MinValue;
+      }
     }
 
     public string GetDeviceId()
