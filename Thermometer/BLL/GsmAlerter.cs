0b4eb4d06aa30ceb7bdfb059fe2f58e871a69c5b
RSzkorla <r.szkorla@gmail.com>
2018-09-10 18:06:28 +0200

    Generating gsm scirpt

--- Thermometer/BLL/GsmAlerter.cs
+++ Thermometer/BLL/GsmAlerter.cs
@@ -1,31 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Thermometer.BLL
 {
-  public class GsmAlerter:IAlerter
-    {
-
-    private IHostingEnvironment _env;
+  public class GsmAlerter : IAlerter
+  {
+    private readonly IHostingEnvironment _env;
     public bool CanISendAlert = true;
-    public List<string> PhoneNumberList { get; set; }
 
     public GsmAlerter()
     {
       _env = Startup.Environment;
-      PhoneNumberList = new List<string>()
+      PhoneNumberList = new List<string>
       {
         "+48883984162"
       };
     }
+
+    public List<string> PhoneNumberList { get; set; }
+
     public void SendWarning(string message)
     {
-      
     }
 
     public void SendAlert(string message)
@@ -33,24 +30,24 @@ namespace Thermometer.BLL
       if (CanISendAlert)
       {
         var strb = new StringBuilder();
+        strb.AppendLine("#!/usr/bin/expect -f");
+        strb.AppendLine("spawn minicom -D /dev/ttyS0\\r\"");
+        strb.AppendLine("send \"AT\\r\"");
+        strb.AppendLine("expect");
         foreach (var number in PhoneNumberList)
         {
-          //  strb.AppendLine("#!/usr/bin/expect -f");
-          //  strb.AppendLine("spawn minicom -D /dev/ttyS0\\r\"");
-          //  strb.AppendLine("send \"AT\\r\"");
-          //  strb.AppendLine("expect");
-          //  strb.AppendLine("send \"AT + CMGF = 1\\r\"");
-          //  strb.AppendLine("expect");
-          //  strb.AppendLine($"send \"AT+CMGS=\\\"{number}\\\"\\r\"");
-          //  strb.AppendLine("expect");
-          //  strb.AppendLine($"send \"{message}\r\"");
-          //  strb.AppendLine("expect");
-          //  strb.AppendLine("send \"\032\"");
-          //  File.WriteAllText(Path.Combine(_env.WebRootPath, "bashScripts", "script.sh"), strb.ToString());
-          //  var path = Path.Combine(_env.WebRootPath, "bashScripts", "script.sh");
-          var path = Path.Combine("/home", "pi", "script.sh");
-        path.Bash();
+          strb.AppendLine("send \"AT + CMGF = 1\\r\"");
+          strb.AppendLine("expect");
+          strb.AppendLine($"send \"AT+CMGS=\\\"{number}\\\"\\r\"");
+          strb.AppendLine("expect");
+          strb.AppendLine($"send \"{message}\r\"");
+          strb.AppendLine("expect");
+          strb.AppendLine("send \"\032\"");
         }
+        File.WriteAllText(Path.Combine("/home","pi", "script.sh"), strb.ToString());
+        var path = Path.Combine(_env.WebRootPath, "bashScripts", "script.sh");
+        //var path = Path.Combine("/home", "pi", "script.sh");
+        path.Bash();
       }
     }
   }
