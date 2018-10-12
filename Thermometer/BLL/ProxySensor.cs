fd3572e73e54f5ad782da9a540b7b334c462d5e4
RSzkorla <r.szkorla@gmail.com>
2018-10-12 13:01:54 +0200

    Improving ProxySensor

--- Thermometer/BLL/ProxySensor.cs
+++ Thermometer/BLL/ProxySensor.cs
@@ -7,9 +7,17 @@ namespace Thermometer.BLL
   {
     private double _lowerRngBorder = 10.0;
     private double _upperRngBorder = 25.0;
+    private int _functionParam = 0;
+    private int _functionInrement = 1;
     private readonly string _id= Guid.NewGuid().ToString();
     private static readonly Random _rng = new Random();
-    public double GetTemperature() => Math.Round((_rng.NextDouble() * (_upperRngBorder - _lowerRngBorder) + _lowerRngBorder), 3);
+    public double GetTemperature()
+    {
+      _functionParam += _functionInrement;
+      if (_functionParam == 0) _functionInrement *= -1;
+      if (_functionParam == 10) _functionInrement *= -1;
+      return Math.Round((_functionParam*Math.Sin(_functionParam)+_lowerRngBorder), 3);
+    }
 
     public string GetDeviceId() => _id.Take(5).ToString();
 
