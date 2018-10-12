cd64061074cedf642bfbfce569c11d810af771dc
RSzkorla <r.szkorla@gmail.com>
2018-10-12 13:32:33 +0200

    Preparing for multiple diagrams

--- Thermometer/Models/ConfigViewModel.cs
+++ Thermometer/Models/ConfigViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Thermometer.BLL;
 
 namespace Thermometer.Models
 {
@@ -11,6 +12,8 @@ namespace Thermometer.Models
     public double LowerWarnBorder { get; set; }
     public double UpperWarnBorder { get; set; }
     public double UpperAlarmBorder { get; set; }
+
+    public double CountOfSensors = Engine.Sensors.Count;
 
     public List<string> Emails { get; set; }
     public List<string> PhoneNumbers { get; set; }
