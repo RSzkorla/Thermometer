bda3bc9b22e7eee68d89fc73059b31af558bb6bf
RSzkorla <r.szkorla@gmail.com>
2019-01-02 13:39:41 +0100

    TimeStamp Class

---
+++ Thermometer/BLL/TimeStamp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Thermometer.BLL
+{
+  public class TimeStamp
+  {
+    public DateTime Time { get; set; }
+    public string Type { get; set; }
+    public string Value { get; set; }
+
+    public TimeStamp(string type,string value)
+    {
+      Time = DateTime.Now;
+      Type = type;
+      Value = value;
+    }
+
+    public override string ToString()
+    {
+      return Time.ToShortDateString() + " "
+             + Time.ToShortTimeString() + " "
+             + Type + ":"
+             + Value;
+    }
+  }
+}
