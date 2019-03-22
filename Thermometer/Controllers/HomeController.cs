f02b3f775377b4979aea338a6c87367b6f199fad
RSzkorla <r.szkorla@gmail.com>
2019-03-22 14:17:35 +0100

    Reports download

--- Thermometer/Controllers/HomeController.cs
+++ Thermometer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -49,5 +50,55 @@ namespace Thermometer.Controllers
       
       return RedirectToAction("Index", "Home",new { message = "Ok" });
     }
+
+    public IActionResult Reports()
+    {
+      var files = Directory.GetFiles(Path.Combine(Path.Combine(Environment.CurrentDirectory, "Reports"))).ToList();
+      ViewBag.Files = files;
+      return View(files);
+    }
+
+    public async Task<IActionResult> GetFile(string fileName)
+    {
+      if (fileName == null)
+        return Content("filename not present");
+
+      var path = Path.Combine(
+        Directory.GetCurrentDirectory(),
+        "wwwroot", fileName);
+
+      var memory = new MemoryStream();
+      using (var stream = new FileStream(path, FileMode.Open))
+      {
+        await stream.CopyToAsync(memory);
+      }
+      memory.Position = 0;
+      return File(memory, GetContentType(path), Path.GetFileName(path));
+    }
+    private string GetContentType(string path)
+    {
+      var types = GetMimeTypes();
+      var ext = Path.GetExtension(path).ToLowerInvariant();
+      return types[ext];
+    }
+
+    private Dictionary<string, string> GetMimeTypes()
+    {
+      return new Dictionary<string, string>
+      {
+        {".txt", "text/plain"},
+        {".pdf", "application/pdf"},
+        {".doc", "application/vnd.ms-word"},
+        {".docx", "application/vnd.ms-word"},
+        {".xls", "application/vnd.ms-excel"},
+        {".xlsx", "application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet"},  
+          {".png", "image/png"},
+        {".jpg", "image/jpeg"},
+        {".jpeg", "image/jpeg"},
+        {".gif", "image/gif"},
+        {".csv", "text/csv"}
+      };
+      }
+
   }
 }
